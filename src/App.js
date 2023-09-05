05daabce2fb5a16534b92bf47361399a8f409cb7
sumonaung23 <sumonaung575@gmail.com>
2023-09-05 15:13:30 +0630

    created files inside components folder then exported and imported with App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'''
+
 const App = () => {
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000'}}>
