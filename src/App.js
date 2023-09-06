524ef7852568a824e89231ba93034222404bbd0d
sumonaung23 <sumonaung575@gmail.com>
2023-09-06 23:52:45 +0630

    modified App.js and Feed.jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
