f53b9a94d229b052de4d2c48b74fbd9973013537
sumonaung23 <sumonaung575@gmail.com>
2023-09-06 23:57:22 +0630

    modified Feed.jsx by adding Typography section and some properties

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,8 +7,16 @@ const Feed = () => {
         <Stack sx={
             { flexDirection: { sx: 'column', md: 'row'}}
         }>
-            <Box sx={{ height: { sx: 'auto', md: '92vh' }}}>
+            <Box sx={{ height: { sx: 'auto', md: '92vh'}, 
+                borderRight: '1px solid #3d3d3d',
+                px: { sx: 0, md: 2 }}}>
+                    Sidebar
 
+                    <Typography className='copyright'
+                    variant='body2' sx={{ mt: 1.5,
+                        color: '#fff'}}>
+                        Copyright 2023 JSM Media
+                    </Typography>
             </Box>
         </Stack>
     )
