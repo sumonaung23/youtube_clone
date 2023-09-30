36b96bf556207444f38af59297a624569ffe65dd
sumonaung23 <sumonaung575@gmail.com>
2023-09-30 22:34:21 +0630

    created sidebar and videos then added in index.jsx and feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Videos from './Videos';
+import { Sidebar, Videos } from './';
 
 
 const Feed = () => {
@@ -11,7 +11,7 @@ const Feed = () => {
             <Box sx={{ height: { sx: 'auto', md: '92vh'}, 
                 borderRight: '1px solid #3d3d3d',
                 px: { sx: 0, md: 2 }}}>
-                    Sidebar
+                    <Sidebar />
 
                     <Typography className='copyright'
                     variant='body2' sx={{ mt: 1.5,
