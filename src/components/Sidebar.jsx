5853e927ceb2a8e84eaf9a08609295204792ea95
sumonaung23 <sumonaung575@gmail.com>
2023-09-30 23:53:00 +0630

    added sidebar features with Stack and used map method to display categories on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material'
+import { categories } from '../utils/constants';
 
-const SideBar = () => {
-    return (
-        <div>SideBar</div>
-    )
-}
+const selectedCategory = 'New';
+
+const SideBar = () => (
+    <Stack
+        direction="row"
+        sx={{
+            overflowY: 'auto',
+            height: { sx: 'auto', md: '92vh'},
+            flexDirection: { md: 'column'}
+        }}
+    >
+
+        {categories.map((category) => (
+            <button
+                className='category-btn'
+                style={{
+                    background: category.name === selectedCategory &&
+                    '#FC1503',
+                    color: 'white'
+                }}
+                key={category.name}
+            >
+                <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+
+                <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+            </button>
+        ))}
+
+    </Stack>
+)
 
 export default SideBar
