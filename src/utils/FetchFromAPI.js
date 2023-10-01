fdb90d3014eca7f1b0fa773376c45d0744e1931a
sumonaung23 <sumonaung575@gmail.com>
2023-10-01 14:37:14 +0630

    modified the url for fixing the bug before and after subscribing API

--- src/utils/FetchFromAPI.js
+++ src/utils/FetchFromAPI.js
@@ -1,28 +1,24 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
+    method: 'get',
+    url: BASE_URL,
     params: {
-        /* part: 'snippet',
-        videoId: 'M7FIvfx5J10', */
-        maxResults: '50'
+        part: 'snippet',
+        videoId: 'M7FIvfx5J10',
+        maxResults: '80'
     },
     headers: {
         /* 'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402', */
         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
-};
-
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-} catch (error) {
-        console.error(error);
 }
 
-export const fetchFromApi = async(url) => {
+export const fetchFromAPI = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
     return data;
-}
+};
