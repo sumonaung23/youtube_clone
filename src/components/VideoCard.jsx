d5b057e2e260ae1d22cb2fccf3f566946501049d
sumonaung23 <sumonaung575@gmail.com>
2023-10-05 23:31:57 +0630

    modified styles

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,7 @@ const VideoCard = ({ video: {id: {videoId}, snippet} }) => {
                 <CardMedia 
                     image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
                     alt={snippet?.title}
-                    sx={{width: 358, height: 180}}/>
+                    sx={{width: {xs: '100%', sm: '358px'}, height: 180}}/>
             </Link>
 
             <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
